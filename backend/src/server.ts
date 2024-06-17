82a60b3d9c0a9f7ceb59de378f4ce486455a7c59
Shui Shan <shui.clouds@gmail.com>
2024-06-17 10:46:18 +0100

    code clean up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-
-const a = 'hello';
-
+ 
 app.get('/', (request, response) => {
 	response.send('Hello World!');
 	console.log('Hello World!');
